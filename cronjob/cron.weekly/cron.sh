2a8d5063c826eb5958d933476aee10c413f3cf22
Priyanka Singh <Rakhee@Iris3.local>
2016-08-04 14:45:08 +0100

    Cleaned the shell script

--- cronjob/cron.weekly/cron.sh
+++ cronjob/cron.weekly/cron.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
-GIT_DIR = /tmp/fellows
-
-REPO_DIR = "visualisation"
-
+GIT_DIR = /tmp/visualisation_repo
 DATA_DIR = /Public/Data/Fellows
 
+#Delete the repository directory and recreate
+rm -rf $GIT_DIR
+mkdir -p $GIT_DIR
 
-#making Data directory
-if [ ! -d "$DATA_DIR" ]; then mkdir $GIT_DIR; f
-
-#Delete the repository if it exist
 cd $GIT_DIR
-rm -rf $REPO_DIR
-
-
-#making repos directory
-if [ ! -d "$GIT_DIR" ]; then mkdir $GIT_DIR; fi
-
-if [ ! -d "$GIT_DIR/$REPO_DIR" ]; then
-    init=true
-    cd $GIT_DIR && pwd && git clone https://github.com/softwaresaved/visualisation.git && cd $REPO_DIR
-#else
-#    cd $GIT_DIR/$REPO_DIR
-#    git checkout master && git up
-
-fi
-
-echo pwd
+git clone https://github.com/softwaresaved/visualisation.git
+cd visualisation
 
 #Run the shell scripts in git repository
 
@@ -36,8 +18,3 @@ source get_consultancy_data.sh 1Jmq6ongECyJih7HbroltnAGZOecWOsl1qFdemw8Hj8A $DAT
 
 # run swc shell script
 source get_swc_data.sh 1xna48IRFl-lLJrPhlI7vC-1TWnl1kICxzEfrvpIKLp8 $DATA_DIR
-
-
-#Delete the repository
-cd $GIT_DIR
-rm -rf $REPO_DIR
